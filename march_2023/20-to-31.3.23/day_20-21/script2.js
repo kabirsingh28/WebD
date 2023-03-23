aed3e0a137ac2e8795f15caf160c0c3cf82e2b25
kabirsingh28 <7darkshades@gmail.com>
2023-03-23 21:29:46 +0530

    Day-21 || Javascript variables, operators, Data-Types with examples

---
+++ march_2023/20-to-31.3.23/day_20-21/script2.js
@@ -0,0 +1,18 @@
+const n1 = 34;
+const n2 = 89;
+
+let result = n1 + n2;
+
+document.getElementById("para").innerHTML = "result is = "+ result;
+
+var string1 = "Kabir";
+var string2 = "brainy";
+
+document.write(`${string1} is handsome and ${string2}`);
+
+let x = "Kabir " + 28;
+
+document.write(`\t + ${x}`);
+
+
+
