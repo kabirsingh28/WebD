229e772abec2b9b19ee2917df0310ba95cf91d83
kabirsingh28 <7darkshades@gmail.com>
2023-03-26 01:51:41 +0530

    Day-23 || Javascript array, objects, class with constructor with examples

--- march_2023/20-to-31.3.23/day_22-23/script.js
+++ march_2023/20-to-31.3.23/day_22-23/script.js
@@ -1,8 +1,40 @@
-function example(n1,n2){
-    var a = n1 + " Kabir " + n2;
-    return a;
+function example(n1, n2) {
+  var a = n1 + " Kabir " + n2;
+  return a;
 }
 
-var x = example("This side", "singh")
+var x = example("This side", "singh");
 document.getElementById("beta").innerHTML = x;
- 
+
+const names = ["kabir", "singh", "hanspal"];
+
+var data = {
+  name: "Kabir",
+  age: 12,
+  isadult: true,
+};
+document.write(names);
+document.write(`\t${data.name} is ${data.age} years old`);
+
+var siblings = ["Kabir", "Geetu", "Vani", "sunit", "amayra"];
+
+siblings[2] = "Amayra";
+
+document.getElementById("beta_2").innerHTML = siblings[siblings.length - 1];
+
+function nam(x,y) {
+    return "my name is " + x + " " + y;
+}
+var x = prompt("first name?");
+var y = prompt("last name?");
+document.write(nam(x,y));
+
+function user(first, last){
+    this.first = first;
+    this.last = last;
+}
+
+var user1 = new user("kabir","singh");
+var user2 = new user("geetu","bhullar");
+
+document.write(user2.last);
