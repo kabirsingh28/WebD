943876079572b312b01ba57ccfc9ff6c65bdaf17
kabirsingh28 <7darkshades@gmail.com>
2023-03-28 23:29:42 +0530

    gonna finish day 24 to 25 assigned tasks

---
+++ march_2023/20-to-31.3.23/day_24-25/script.js
@@ -0,0 +1,61 @@
+// function hello(first,last){
+//      this.first = first;
+//      this.last = last;
+// }
+
+// var x = prompt("hello, whats your first name?");
+// var y = prompt("last name?");
+// var t = new hello(x,y);
+// document.getElementById("beta").innerHTML = "Hello " + t.first + " " + t.last;
+
+// const solution = new Function('x','y', 'return x+y');
+// document.getElementById("beta").innerHTML = solution(24,56);
+
+// let Name = {
+//     firstname: 'Kabir',
+//     lastname: 'Singh'
+// };
+
+// document.getElementById("beta").innerHTML = "Hello "+ Name.firstname + " " + Name.lastname;
+
+// function Name(firstname, lastname){
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+// }
+
+// var name1 = new Name('Kabir', 'singh');
+// document.getElementById("beta").innerHTML = name1.firstname;
+
+// function Fruits(name,color){
+//     this.name = name;
+//     this.color = color;
+// }
+
+// const fruit1 = new Fruits('kiwi','brown');
+// document.getElementById("beta").innerHTML = fruit1.name + " is " + fruit1.color;
+
+// function watch(brand, year) {
+//   this.brand = brand;
+//   this.year = year;
+// }
+
+// const watch1 = new watch("rado", 2000);
+// const watch2 = new watch("quartz", 2005);
+// watch1.color = "red";
+// console.log(
+//   "I have 2 watches: " +
+//     watch1.brand +
+//     " and " +
+//     watch2.brand +
+//     " of year " +
+//     watch1.year +
+//     " and " +
+//     watch2.year +
+//     " respectively" +
+//     " and " +
+//     watch1.brand +
+//     " is of " +
+//     watch1.color +
+//     " color"
+// );
+
